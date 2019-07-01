4b61c1d73519f5c3ffd8ac80adccfe8cfb993d0c
matthew-schiller <maugustschiller@gmail.com>
2019-07-01 14:59:42 -0700

    users

---
+++ 00-pre-mean/01-js-fundamentals/06-users.js
@@ -0,0 +1,51 @@
+function userLanguages(users) {
+  var interestedIn = [];
+  for (var i = 0; i < users.length; i++) {
+    var toon = users[i];
+    var interests = toon.interests;
+    for (var k in interests) {
+      var ints = interests[k];
+      for (var k = 0; k < ints.length; k++) {
+        interestedIn.push(ints[k]);
+      }
+    }
+    console.log(toon.fname + " " + toon.lname + " knows " + toon.languages);
+    var pop = interestedIn.pop();
+    console.log(toon.fname + " " + toon.lname + " is also interested in " + interestedIn.join(",") + " and " + pop + "\n");
+  }
+}
+
+
+var users = [
+  {
+    fname: "Kermit",
+    lname: "the Frog",
+    languages: ["Python", "JavaScript", "C#", "HTML", "CSS", "SQL"],
+    interests: {
+      music: ["guitar", "flute"],
+      dance: ["tap", "salsa"],
+      television: ["Black Mirror", "Stranger Things"]
+    }
+  },
+  {
+    fname: "Winnie",
+    lname: "the Pooh",
+    languages: ["Python", "Swift", "Java"],
+    interests: {
+      food: ["honey", "honeycomb"],
+      flowers: ["honeysuckle"],
+      mysteries: ["Heffalumps"]
+    }
+  },
+  {
+    fname: "Arthur",
+    lname: "Dent",
+    languages: ["JavaScript", "HTML", "Go"],
+    interests: {
+      space: ["stars", "planets", "improbability"],
+      home: ["tea", "yellow bulldozers"]
+    }
+  }
+]
+
+userLanguages(users);
