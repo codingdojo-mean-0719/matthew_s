e61913a4196d1e2f63ce85c3a61d313f5b892844
matthew-schiller <maugustschiller@gmail.com>
2019-07-03 22:54:53 -0700

    ninjas 2

---
+++ 01-javascript/03-js-oop/03-ninja-class-iii.js
@@ -0,0 +1,34 @@
+class Ninja {
+  constructor(name, health, speed, strength) {
+    this.name = name;
+    this.health = 100;
+    this.speed = 3;
+    this.strength = 3;
+  }
+
+  sayName() {
+    console.log("Hi! My name is " + this.name);
+  }
+  showStats() {
+    console.log(`Name: ${this.name} Health: ${this.health} Speed: ${this.speed} Strength: ${this.strength}`);
+  }
+  drinkSake() {
+    this.health += 10;
+  }
+}
+
+class Sensei extends Ninja {
+  constructor(wisdom) {
+    this.health = 200;
+    this.speed = 10;
+    this.strength = 10;
+    this.widsom = 10;
+  }
+  drinkAndSpeak() {
+    super.drinkSake();
+    console.log("ahhh that was good sake, I feel healthier");
+  }
+}
+
+const superSensei = new Sensei("Master Splinter");
+superSensei.drinkAndSpeak();
