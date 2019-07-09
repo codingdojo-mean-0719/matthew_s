05a6d74a87f48523cc770c40193eb35b3df7e3af
matthew-schiller <maugustschiller@gmail.com>
2019-07-08 19:33:43 -0700

    landing page

---
+++ 02-node/01-node/01-landing-page/app.js
@@ -0,0 +1,33 @@
+var http = require('http');
+var fs = require('fs');
+var server = http.createServer(function (request, response) {
+  console.log('client request URL: ', request.url);
+  if (request.url === '/') {
+    fs.readFile('index.html', 'utf8', function (errors, contents) {
+      response.writeHead(200, { 'Content-Type': 'text/html' });  // send data about response
+      response.write(contents);  //  send response body
+      response.end(); // finished!
+    });
+  } else if (request.url === '/ninjas') {
+    fs.readFile('ninja.html', 'utf8', function (errors, contents) {
+      response.writeHead(200, { 'Content-type': 'text/html' });
+      response.write(contents);
+      response.end();
+    });
+  } else if (request.url === "/dojos/new") {
+    fs.readFile('dojos.html', 'utf8', function (errors, contents) {
+      response.writeHead(200, { 'Content-type': 'text/html' });
+      response.write(contents);
+      response.end();
+    })
+  } else {
+    fs.readFile('error.html', 'utf8', function (errors, contents) {
+      response.writeHead(404, { 'Content-type': 'text/html' });
+      response.write(contents);
+      response.end();
+    })
+  }
+});
+server.listen(6789);
+
+console.log("Running in localhost at port 6789");
