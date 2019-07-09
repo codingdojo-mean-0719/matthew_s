d4296bd5ba77c684d13963deada044314e543032
matthew-schiller <maugustschiller@gmail.com>
2019-07-09 10:55:28 -0700

    cars and cats

---
+++ 03-express/01-express.js/01-cars-and-cats/server.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+app.use(express.static(__dirname + "/static"));
+app.get('/', function (request, response) {
+  // just for fun, take a look at the request and response objects
+  console.log("The request object", request);
+  console.log("The response object", response);
+  // use the response object's .send() method to respond with an h1
+  response.send("<h1>Hello Express</h1>");
+})
+// tell the express app to listen on port 8000, always put this at the end of your server.js file
+app.listen(8000, function () {
+  console.log("listening on port 8000");
+})
