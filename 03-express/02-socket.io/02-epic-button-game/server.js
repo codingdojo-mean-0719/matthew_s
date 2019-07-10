55dcb2cc6f7334551e63905054aadeb6c55595d9
matthew-schiller <maugustschiller@gmail.com>
2019-07-09 21:23:05 -0700

    absolutely epic button game

---
+++ 03-express/02-socket.io/02-epic-button-game/server.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const app = express();
+app.set('views', __dirname + '/public');
+const server = app.listen(1337);
+const io = require('socket.io')(server);
+app.set('view engine', 'ejs');
+
+var counter = 0
+io.on('connection', function (socket) {
+  socket.emit('greeting', { msg: 'Sup wit it, vanilla face?' });
+  socket.on('button_click', function (data) {
+    counter += data.counter;
+    console.log(counter);
+    //Emit Updated Counter    
+    socket.emit('updated_counter', { counter: counter });
+  });
+  socket.on('reset_click', function () {
+    counter = 0;
+    socket.emit('updated_counter', { counter: counter });
+  });
+});
+
+app.get('/', function (request, response) {
+  response.render('index');
+})
+
+app.listen(8000, function () {
+  console.log("listening on port 8000");
+})
