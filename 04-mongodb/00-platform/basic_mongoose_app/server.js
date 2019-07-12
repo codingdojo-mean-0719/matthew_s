3bf0cdd12c680d67e109a757de031886667e303a
matthew-schiller <maugustschiller@gmail.com>
2019-07-12 13:20:18 -0700

    quoting dojo compete! quote me on that

--- 04-mongodb/00-platform/basic_mongoose_app/server.js
+++ 04-mongodb/00-platform/basic_mongoose_app/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const app = express();
+var session = require('express-session');
 var bodyParser = require('body-parser')
 app.use(bodyParser.urlencoded({ extended: true }));
 app.set('views', __dirname + '/public');
@@ -9,6 +10,12 @@ app.set('view engine', 'ejs');
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/basic_mongoose');
 mongoose.Promise = global.Promise;
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
 
 var UserSchema = new mongoose.Schema({
   name: String,
@@ -19,15 +26,14 @@ var User = mongoose.model('User');
 
 app.get('/', function (request, response) {
   let users = User.find({}, function (err, users) {
-
   })
   response.render('index', { user: users });
 });
 
 app.post('/users', function (req, res) {
   console.log("Post Data", req.body);
-  var user = new User({ name: req.body.name, age: req.body.age });
-  user.save(function (err) {
+  var quote = new Quote({ name: req.body.name, age: req.body.age });
+  quote.save(function (err) {
     if (err) {
       console.log('something aint right');
     } else {
---
+++ 04-mongodb/02-mongoose/01-quoting-dojo/server.js
@@ -0,0 +1,61 @@
+const express = require('express');
+const app = express();
+var bodyParser = require('body-parser');
+var session = require('express-session');
+app.use(bodyParser.urlencoded({ extended: true }));
+app.set('views', __dirname + '/public');
+const server = app.listen(1337);
+app.use(express.static(__dirname + "/static"));
+app.set('view engine', 'ejs');
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/basic_mongoose');
+mongoose.Promise = global.Promise;
+const flash = require('express-flash');
+app.use(flash());
+app.use(session({
+  secret: 'keyboardkitteh',
+  resave: false,
+  saveUninitialized: true,
+  cookie: { maxAge: 60000 }
+}))
+
+var quoteSchema = new mongoose.Schema({
+  name: { type: String, required: true, minlength: 2, message: "name is required homie" },
+  quote: { type: String, required: true, message: "quote is required dude!" }
+},
+  { timestamps: true }
+);
+
+mongoose.model('Quote', quoteSchema);
+var Quote = mongoose.model('Quote');
+
+app.get('/', function (request, response) {
+
+  response.render('index');
+});
+
+app.post('/quotes', function (req, res) {
+  console.log("Post Data", req.body);
+  var quote = new Quote(req.body);
+  quote.save(function (err) {
+    if (err) {
+      console.log('something aint right');
+      for (var key in err.errors) {
+        req.flash('registration', err.errors[key].message);
+      }
+    } else {
+      console.log("homie is in the db now!");
+    }
+    res.redirect("/");
+  })
+});
+
+app.get('/quotes', function (req, res) {
+  Quote.find({})
+    .then(results => {
+      res.render('quotes', { quotes: results });
+    })
+    .catch(err => {
+      console.log("error fetching data:", err)
+    });
+})
